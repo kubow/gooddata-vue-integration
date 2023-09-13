08b8a21efa7b33daf221ac3e6fccd1f9e49142cb
Kube Kubow <kubow@users.noreply.github.com>
2023-09-13 09:38:43 +0200

    first working prototype

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script setup>
+    import * as Vue from 'vue';     //import Vue from 'vue';
+    import 'https://demos.cloud.gooddata.com/components/ecommerce-parent-aj-jaurigue.js?auth=sso';
+  //import vueCustomElement from 'vue-custom-element';
+  //import { Dashboard } from 'https://demos.cloud.gooddata.com/components/ecommerce-parent-aj-jaurigue.js?auth=sso';
+  //import {GoodInsight, GoodDashboard} from 'https://demos.cloud.gooddata.com/components/ecommerce-parent-aj-jaurigue.js?auth=sso';
+
+/*  import HelloWorld from './components/HelloWorld.vue';
+    import TheWelcome from './components/TheWelcome.vue'; */
+//  import WebComponent from './components/WebComponentWrapper.vue';
+
+  //Vue.use(vueCustomElement);
+  //Vue.customElement('<gd-insight', Insight);
+  //Vue.customElement('<gd-dashboard', Dashboard);
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  </header>
+
+  <main>
+    <div class="wrapper">
+      <p>First the dashboard:</p>
+      <gd-dashboard dashboard="092929af-375a-4e9c-964f-2add8cdbd259"></gd-dashboard>
+      <p>Then an Insight:</p>
+      <gd-insight insight="f1027458-1123-4f4d-af20-0e21b1d5e009" title="Hourly Activity" style="height: 400px"></gd-insight>
+    <!--   <HelloWorld msg="You did it!" /> -->
+    </div>
+    <!-- <TheWelcome /> -->
+    <!-- <gd-insight insight="543cadbb-10b4-4153-b810-52586df7aa0e"></gd-insight> -->
+    <!-- <WebComponent></WebComponent> -->
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/WebComponentWrapper.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+      <!-- ...Your other Vue.js components here... -->
+        Here
+      <!-- Placeholder for the web components -->
+      <div ref="webComponentsContainer"></div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    mounted() {
+      // Load the web component script dynamically
+      const script = document.createElement('script');
+      script.src = 'https://demos.cloud.gooddata.com/components/ecommerce-parent-aj-jaurigue.js?auth=sso';
+      script.defer = true;
+      script.onload = this.initializeWebComponents;
+      document.body.appendChild(script);
+    },
+    methods: {
+      initializeWebComponents() {
+        // Assuming the web components are globally registered by the script
+        // Access the web components using their tag names
+        const gdDashboard = document.createElement('gd-dashboard');
+        gdDashboard.setAttribute('dashboard', '092929af-375a-4e9c-964f-2add8cdbd259');
+        gdDashboard.style.height = '700px';
+        gdDashboard.style.width = '1024px';
+  
+        const gdInsight = document.createElement('gd-insight');
+        gdInsight.setAttribute('insight', '543cadbb-10b4-4153-b810-52586df7aa0e');
+        gdInsight.setAttribute('workspace', '4248');
+        gdInsight.setAttribute('locale', 'en_US');
+        gdInsight.setAttribute('title', 'Custom insight title');
+  
+        // Append the web components to the container element
+        const container = this.$refs.webComponentsContainer;
+        container.appendChild(gdDashboard);
+        container.appendChild(gdInsight);
+      },
+    },
+  };
+  </script>
