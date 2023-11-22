654cc65a968ba08d1a12e633f785ba834777d03c
Kube Kubow <kubow@users.noreply.github.com>
2023-11-22 13:55:52 +0100

    custom wrapper component #1 attempt

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
   // import DefaultGoodDataComponent from './components/DefaultGoodDataComponent.vue';
   import TokenAuthComponent from './components/TokenAuthComponent.vue';
+  // import CustomWrapperComponent from './components/CustomWrapperComponent.vue';
 </script>
 
 <template>
   <!-- <DefaultGoodDataComponent /> -->
   <TokenAuthComponent />
+  <!-- <CustomWrapperComponent />-->
 </template>
 
 <style scoped>
