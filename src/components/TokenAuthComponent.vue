5692cefdf5c83f2bc583e0e36897aebc75e8be20
kubow <kube.kubow@gmail.com>
2024-09-17 11:06:47 +0200

    attempt to do token auth component

--- src/components/TokenAuthComponent.vue
+++ src/components/TokenAuthComponent.vue
@@ -1,13 +1,33 @@
 <script setup>
-  import { setContext } from 'https://demos.cloud.gooddata.com/components/ecommerce-parent-dev.js'; 
-  import factory, { TigerTokenAuthProvider } from "https://demos.cloud.gooddata.com/components/tigerBackend.js";
+  import { onMounted } from 'vue';
 
-  setContext({
-    backend: factory()
-      .onHostname("https://____.cloud.gooddata.com")
-      .withAuthentication(new TigerTokenAuthProvider("__your_token__")),
-    workspaceId: "__your_workspace_id__",
-  });
+    onMounted(async () => {
+        // Construct the first module URL using environment variables
+        const wsUrl = `${import.meta.env.VITE_HOST}components/${import.meta.env.VITE_WORKSPACE}.js`;
+        const beUrl = `${import.meta.env.VITE_HOST}components/tigerBackend.js`
+
+        try {
+            // Dynamically import the modules using the constructed URL and static URL
+            const { setContext } = await import(/* @vite-ignore */ wsUrl);
+            const { default: factory, TigerTokenAuthProvider } = await import(/* @vite-ignore */ beUrl);
+            console.log("Imported well, no error above?, now settings context")
+
+            // Use the imported functions to set the context
+            setContext({
+            backend: factory()
+                .onHostname(import.meta.env.VITE_HOST) // Use env variable for hostname
+                .withAuthentication(new TigerTokenAuthProvider(import.meta.env.VITE_TOKEN)), // Use env variable for token
+            workspaceId: import.meta.env.VITE_WORKSPACE, // Use env variable for workspace ID
+            });
+            console.log("Also succesull, now using components...")
+        } catch (error) {
+            console.error('Error loading modules:', error);
+        }
+    });
+
+    const dashboardId = import.meta.env.VITE_DASHBOARD;
+    const visualId = import.meta.env.VITE_VISUALIZATION;
+    console.log(`${dashboardId} / ${visualId}`);
 
 </script>
 
@@ -16,7 +36,7 @@
         <div id="left">
             <p>Left side with an Insight:</p>
             <gd-insight 
-                insight="__your_visual_id__"
+                :insight="visualId"
                 title="top 10 performers" 
                 style="height: 700px;"
             >
@@ -25,11 +45,11 @@
         <div id="right">
             <p>Right side with the dashboard:</p>
             <gd-dashboard 
-                dashboard="__your_dashboard_id__"
+                :dashboard="dashboardId"
             >
             </gd-dashboard>
         </div>
-  </div>
+    </div>
 </template>
 
 <style scoped>
